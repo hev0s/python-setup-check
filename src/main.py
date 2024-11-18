575f86e76b6efb4ae5e312ca6d354ec2442b8a70
Niels Delafontaine <pa76wlu@eduvaud.ch>
2024-11-18 12:05:39 +0100

    refactor: Externalisation de la fonction ask_user_input

--- src/main.py
+++ src/main.py
@@ -1,32 +1,52 @@
-def main():
-    # Get first operand from the user
-    operand1 = float(input("Enter the first operand: "))
+operator = None
+operand1 = None
+operand2 = None
+result = None
 
+def main():
+    ask_user_input()
+    calcul()
+    resultat()
+
+
+def ask_user_input():
+    # Get first operand from the user
+    global operand1, operator, operand2
+    operand1 = float(input("Enter the first operand: "))
     # Get the operator from the user
     operator = input("Enter an operator (+, -, *, /): ")
-
     # Get second operand from the user
     operand2 = float(input("Enter the second operand: "))
 
-    # Perform the operation based on the operator
-    if operator == '+':
-        result = operand1 + operand2
-    elif operator == '-':
-        result = operand1 - operand2
-    elif operator == '*':
-        result = operand1 * operand2
-    elif operator == '/':
-        if operand2 == 0:
-            print("Error: Division by zero is undefined.")
-            return
-        result = operand1 / operand2
+
+def calcul():
+    global result
+    match operator:
+        case '+':
+            result = operand1 + operand2
+        case '-':
+            result = operand1 - operand2
+        case '*':
+            result = operand1 * operand2
+
+        case '/':
+            if operand2 != 0:
+                result = operand1 / operand2
+            else:
+                result = None
+        case _:
+            print("Ce n'est pas un +,-,/,*")
+            result = None
+
+def resultat():
+    if result is not None and operator != '/':
+        print(f"Le résultat de {int(operand1)} {operator} {int(operand2)} est {int(result)}")
+    if result is not None and operator == '/':
+        print(f"Le résultat de {int(operand1)} {operator} {int(operand2)} est {result}")
     else:
-        print("Invalid operator.")
-        return
-
-    # Print the result
-    print("Result:", result)
-
-
+        if operator == '/' and operand2 == 0:
+            print("Division par zéro impossible")
+        else:
+            print("Opérateur invalide")
 # Call the main function to run the program
 main()
